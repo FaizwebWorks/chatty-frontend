deb95beb9f9de98bfc8f164cd4a9a27dddf0ba89
Faizal Shaikh <webworksbyfaiz@gmailcom>
2024-12-15 13:42:51 +0530

    some changes

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "/api",
+      : "https://chatty-backend-o38b.onrender.com/api",
   withCredentials: true,
 });
